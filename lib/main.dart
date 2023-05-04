2a528f7fcc10dbefb5f4dd5d190ca50a59e76496
kouseinadaffa <kouseinadaffa@gmail.com>
2023-05-04 21:58:28 +0700

    feat(root): remove unused p;atform & setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login_and_register/firebase_options.dart';
 import 'package:flutter_login_and_register/login.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
 
